1ebb1949fc1995f13be6c3bef6b4f296d87fbaf6
Kaoê Henrik <79760134+kaoehenrik@users.noreply.github.com>
2022-05-02 10:56:51 -0300

    Update Script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function VerificarEntrada() {
     NomeConvidado = document.getElementById('nome').value;
-    ConvidadosKaique = ['kaoe', 'deiverson', 'caio'];
+    // Lista de convidados do Kaique
+    ConvidadosKaique = ['kaue', 'deiverson', 'caio'];
     if (ConvidadosKaique.includes(NomeConvidado)) {
         document.getElementById('PermissaoDeEntrada').innerText = 'Você pode Entrar'
     } else {
         document.getElementById('PermissaoDeEntrada').innerText = 'Você não pode Entrar'
     }
+    // Lista de convidados da Marcela
+    ConvidadosMarcela = ['dani', 'vivi', 'liza'];
+    if (Convidadosmarcela.includes(NomeConvidado)) {
+        document.getElementById('PermissaoDeEntrada').innerText = 'Você pode Entrar'
+    } else {
+        document.getElementById('PermissaoDeEntrada').innerText = 'Você não pode Entrar'
+    }
+    
 }
 
